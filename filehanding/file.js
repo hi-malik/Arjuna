a6c574e32a57afb07c6eb2150699fb8b7712518e
Jaidev Singh Malik <jaidevsinghmalik@Jaidevs-MacBook-Pro.local>
2024-07-11 13:01:53 +0530

    Initalise

---
+++ filehanding/file.js
@@ -0,0 +1,4 @@
+const fs = require("fs")
+const os = require("os")
+
+console.log(os.cpus().length)
--- nodeJs/index.js
+++ nodeJs/index.js
@@ -1,37 +1,103 @@
 import express from "express"
 import path from "path"
+import mongoose from "mongoose"
+import cookieParser from "cookie-parser"
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
 
 const app = express()
 
-const user = []
+// MongoDB Connection
+mongoose.connect("mongodb://localhost:27017", {
+    dbName : "contactForm"
+}).then(() => console.log("DataBase is Connected")).catch((e) => console.log(e))
+
+// Schema
+const userSchema = new mongoose.Schema({
+    name : String,
+    email : String,
+    password : String,
+})
+
+// Collection
+const User = mongoose.model("User", userSchema)
 
 //middleware
 app.set("view engine", "ejs")
 app.use(express.static(path.join(path.resolve(), "public")))
 app.use(express.urlencoded({extended : true}))
+app.use(cookieParser())
+
+const isAunthenticated = async (req, res, next) => {
+    const token = req.cookies.token
+    if(token){
+        const decoded = jwt.verify(token, "asdfghjkjhgfdfgh")
+        req.user = await User.findById(decoded._id)
+        next()
+    }
+    else{
+        res.redirect("/login")
+    }
+}
 
 // API
-app.get("/", (req, res) => {
-    // res.render("index.ejs")
-    res.render("index", {name : "Sita", shame : "Har"})
+app.get("/", isAunthenticated, (req, res) => {
+    res.render("logout", {name : req.user.name})
 })
 
-app.post("/contact", (req, res) => {
-    // console.log(req.body.name)
-    user.push({name : req.body.name, email : req.body.email})
-    res.redirect("success")
+app.get("/logout", (req, res) => {
+    res.cookie("token", null, {
+        httpOnly : true,
+        expires : new Date(Date.now())
+    });
+    res.redirect("/")
 })
 
-app.get("/success", (req, res) => {
-    res.render("success")
+app.get("/register", (req, res) => {
+    res.render("register")
 })
 
-app.get("/userData", (req, res) => {
-    res.json({
-        user,
+app.get("/login", (req, res) => {
+    res.render("login")
+})
+
+app.post("/register", async (req, res) => {
+    const {name, email, password} = req.body
+    let user = await User.findOne({email})
+    if(user){
+        return res.redirect("/login")
+    }
+    const hashPassword = await bcrypt.hash(password, 10)
+    user = await User.create({
+        name,
+        email,
+        password : hashPassword
     })
+    const token = jwt.sign({_id : user._id}, "asdfghjkjhgfdfgh")
+    res.cookie("token", token, {
+        httpOnly : true,
+        expires : new Date(Date.now() + 60 * 1000)
+    });
+    res.redirect("/")
 })
 
+app.post("/login", async (req, res) => {
+    const {email, password} = req.body
+    
+    const user = await User.findOne({email})
+    if(!user) return res.redirect("/register");
+    const isMatch = await bcrypt.compare(password, user.password)
+    if(!isMatch) return res.render("login", {message : "Incorrect Password"})
+    const token = jwt.sign({_id : user._id}, "asdfghjkjhgfdfgh")
+    res.cookie("token", token, {
+        httpOnly : true,
+        expires : new Date(Date.now() + 60 * 1000)
+    });
+    res.redirect("/")
+})
+
+
+// Server
 app.listen(4000, () => {
     console.log("Server is Running")
 })
